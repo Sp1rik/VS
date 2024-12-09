9ab19e59e5ae8926694e7984e66bc45c1b966bfc
Sp1rik <Vitya.Spiridonov08@gmail.com>
2024-12-09 12:51:18 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using System.IO.Pipelines;
+using System.Runtime.ExceptionServices;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите 1 число: ");
+        Double number1 = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Введите 2 число: ");
+        Double number2 = Convert.ToDouble(Console.ReadLine());
+
+        Double result = number1 + number2;
+        Console.WriteLine($"Результат: {result}");
+
+
+    }
+}
